8437f13552393b93d92baf6f2079d17ff04cfba4
rhaesung <rhaesung.kim@noaa.gov>
2023-08-22 19:27:39 +0000

    update JEDI EXEC

--- do_landDA.sh
+++ do_landDA.sh
@@ -28,6 +28,7 @@ echo "reading DA settings from $config_file"
 
 GFSv17=${GFSv17:-"NO"}
 fv3bundle_vn=${fv3bundle_vn:-"psl_develop"}
+#fv3bundle_vn=${fv3bundle_vn:-"release-v1.0"}
 
 source $config_file
 
@@ -315,10 +316,10 @@ echo 'do_landDA: calling fv3-jedi'
 
 if [[ $do_DA == "YES" ]]; then
     if [[ ${BASELINE} =~ 'hera.internal' ]]; then
-    source ${JEDI_EXECDIR}/../../../fv3_mods_hera
+    source /scratch2/NCEPDEV/land/data/jedi/fv3_mods_hera
     srun -n $NPROC_JEDI ${JEDI_EXECDIR}/${JEDI_EXEC} letkf_land.yaml ${LOGDIR}/jedi_letkf.log
     else
-    srun -n $NPROC_JEDI ${JEDI_EXECDIR}/${JEDI_EXEC} letkf_land.yaml ${LOGDIR}/jedi_letkf.log
+    ${MPIEXEC} -n $NPROC_JEDI ${JEDI_EXECDIR}/${JEDI_EXEC} letkf_land.yaml ${LOGDIR}/jedi_letkf.log
     fi
     if [[ $? != 0 ]]; then
         echo "JEDI DA failed"
